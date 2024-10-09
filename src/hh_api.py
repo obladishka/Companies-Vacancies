96548d47d9aa06622921e46594c1a00852a165a7
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-10-09 22:00:11 +0300

    add class for connecting to hh.ru

---
+++ src/hh_api.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+import requests
+
+
+class HHApi:
+    """Родительский класс для работы с платформой hh.ru"""
+
+    url: str
+    headers: dict
+    params: dict
+
+    def __init__(self, url: str) -> None:
+        """Метод для инициализации объектов класса."""
+        self.__url = url
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__params = {}
+
+    def __get_response(self, params: dict[str, Any]) -> None | dict[str, Any]:
+        """Закрытый метод для подключения к API сайта hh.ru."""
+
+        try:
+            self.response = requests.get(self.__url, headers=self.__headers, params=params)
+        except requests.exceptions.RequestException as ex:
+            print(ex)
+        else:
+            if self.response.status_code == 200:
+                return self.response.json()
+
+    def get_response(self, **kwargs) -> None | dict:
+        """Публичный метод для подключения к API сайта hh.ru."""
+        self.__params = dict(**kwargs)
+        return self.__get_response(self.__params)
